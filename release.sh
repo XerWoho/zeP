26d81f3714d8a37c71d4289f084ec44144c172ac
XerWo <realgamerex@gmail.com>
2025-12-07 02:38:36 +0100

    modified release - now releasing multiple targets

--- release.sh
+++ release.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
-zig build -Doptimize=ReleaseFast -freference-trace -Dtarget=x86_64-windows-msvc -p tempR/w
-zig build -Doptimize=ReleaseFast -freference-trace -Dtarget=x86_64-linux-gnu -p tempR/l
-mkdir -p release
-mkdir -p release/w
-mkdir -p release/l
-
 versionName=$1
-if [ $# -eq 0 ]
-  then
-	date=$(date '+%Y-%m-%d')
-	versionName="$date"
+
+if [ $# -eq 0 ]; then
+    versionName=$(date '+%Y-%m-%d')
 fi
 
+mkdir -p release
+mkdir -p tempR
 
-zip -j release/w/windows_$versionName.zip tempR/w/bin/zeP.exe
-zip -r release/w/windows_$versionName.zip packages/
+# Windows targets
+windows_targets=("x86_64-windows" "x86-windows" "aarch64-windows")
 
-tar -C tempR/l/bin -cJf release/l/linux_$versionName.tar.xz zeP ../../../packages/
+for element in "${windows_targets[@]}"; do
+    mkdir -p release/$element
+
+    zig build -Doptimize=ReleaseFast -freference-trace -Dtarget=$element -p tempR/$element
+
+    zip -j release/$element/zep_${element}_$versionName.zip tempR/$element/bin/zeP.exe
+    zip -r release/$element/zep_${element}_$versionName.zip packages/
+done
+
+# Linux targets
+linux_targets=("x86_64-linux" "x86-linux" "aarch64-linux" "x86_64-linux-musl" "aarch64-linux-musl")
+
+for element in "${linux_targets[@]}"; do
+    mkdir -p release/$element
+
+    zig build -Doptimize=ReleaseFast -freference-trace -Dtarget=$element -p tempR/$element
+
+	tar -C tempR/$element/bin -cJf release/$element/zep_${element}_$versionName.tar.xz zeP ../../../packages/
+done
 
 rm -r tempR/
