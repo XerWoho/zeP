000a9afa6a03b7acc2e3a454d18ab112b75fe21e
XerWo <realgamerex@gmail.com>
2025-12-11 01:05:23 +0100

    Added homebrew Formula and fixed PKGBUILD

---
+++ .config/homebrew/zep.rb
@@ -0,0 +1,29 @@
+class Zep < Formula
+  desc "Fast package manager for Zig"
+  homepage "https://github.com/XerWoho/zeP"
+  license "MIT"
+
+  on_macos do
+    url "https://zep.run/releases/0.7/zep_x86_64-macos_0.7.tar.xz"
+    sha256 "ad14f58986b6f81349674ae5ee280060b62a5a6363a644b90177b8e3483b63ac"
+  end
+
+  on_linux do
+    url "https://zep.run/releases/0.7/zep_x86_64-linux_0.7.tar.xz"
+    sha256 "eb349a36c9705f157d1722dc0ea082e11a28569e9c32b6835b642e0cf1b5f598"
+  end
+
+  def install
+    bin.install "zeP" => "zep"
+  end
+
+    def post_install
+		ohai "--- ZEP CONFIG REQUIRED ---"
+		puts " ==> Run 'zeP setup' to configure zeP"
+		puts " ==> Then run: zeP zep install 0.7"
+	end
+
+  test do
+    system "#{bin}/zep", "--version"
+  end
+end
