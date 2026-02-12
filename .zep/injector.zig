4386dbb6e37bc70066c41dfb1e190cdaac77cfe7
XerWo <realgamerex@gmail.com>
2026-02-12 16:22:20 +0100

    fixed versioning on .lock

--- .zep/injector.zig
+++ .zep/injector.zig
@@ -1,15 +1,15 @@
 const std = @import("std");
 pub fn imp(b: *std.Build, exe: *std.Build.Module) void {
- // zon.zig MODULE
- const zon_dep = b.dependency("zon", .{});
- exe.addImport("zon", zon_dep.module("zon"));
- // ----------
- // mvzr MODULE
- const mvzr_dep = b.dependency("mvzr", .{});
- exe.addImport("mvzr", mvzr_dep.module("mvzr"));
- // ----------
- // logly.zig MODULE
- const logly_dep = b.dependency("logly", .{});
- exe.addImport("logly", logly_dep.module("logly"));
- // ----------
+    // zon.zig MODULE
+    const zon_dep = b.dependency("zon", .{});
+    exe.addImport("zon", zon_dep.module("zon"));
+    // ----------
+    // mvzr MODULE
+    const mvzr_dep = b.dependency("mvzr", .{});
+    exe.addImport("mvzr", mvzr_dep.module("mvzr"));
+    // ----------
+    // logly.zig MODULE
+    const logly_dep = b.dependency("logly", .{});
+    exe.addImport("logly", logly_dep.module("logly"));
+    // ----------
 }
